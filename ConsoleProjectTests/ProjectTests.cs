8c3e98454c9a5d114647fefd65f4ef1583961b77
Lammiman <alammiman@Companynet.org>
2018-04-12 14:17:50 +0100

    Added boilerplate console project with unit tests.

---
+++ ConsoleProjectTests/ProjectTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleProjectTests
+{
+    [TestFixture]
+    public class ProjectTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void DummyTestClass()
+        {
+            
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+    }
+}
