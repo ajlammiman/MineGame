7d3e6859d38a2f8899e9de60abde68c3d2a13308
Lammiman <alammiman@Companynet.org>
2018-09-13 14:44:06 +0100

    Added Game test classes into project and added stubs or game tests.

--- ConsoleProjectTests/GameSpecs.cs
+++ ConsoleProjectTests/GameSpecs.cs
@@ -1,6 +1,63 @@
-﻿namespace GameTests
+﻿using NUnit.Framework;
+
+namespace GameTests
 {
+    [TestFixture, Category("Game Tests")]
     public partial class GameSpecs
     {
+        [Test]
+        public void pressing_a_move_left_key_produces_a_move_left()
+        {
+
+        }
+
+        [Test]
+        public void pressing_a_move_right_key_produces_a_move_right()
+        {
+
+        }
+
+        [Test]
+        public void pressing_a_move_up_key_produces_a_move_up()
+        {
+
+        }
+
+        [Test]
+        public void pressing_a_move_down_key_produces_a_move_down()
+        {
+
+        }
+
+        [Test]
+        public void pressing_a_non_move_key_does_not_make_a_move()
+        {
+
+        }
+
+        [Test]
+        public void moving_out_of_bounds_is_not_allowed()
+        {
+
+        }
+
+
+        [Test]
+        public void a_loss_of_life_produces_a_message()
+        {
+
+        }
+
+        [Test]
+        public void a_loss_of_all_lives_ends_the_game()
+        {
+
+        }
+
+        [Test]
+        public void crossing_the_board_wins_the_game()
+        {
+
+        }
     }
 }
